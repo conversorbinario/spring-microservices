5c3ef463c6d0c2d995d2fb5f9dcf0c57a8e273b5
Manuel <manuel.gon.pos@gmail.com>
2024-09-28 14:09:14 +0200

    Retira test contextLoad por el momento

--- src/test/java/com/microservices/microservices/MicroservicesApplicationTests.java
+++ src/test/java/com/microservices/microservices/MicroservicesApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class MicroservicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
 }
