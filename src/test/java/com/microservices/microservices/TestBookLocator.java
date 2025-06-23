9f0eddf0d3a8c558d7471d49415e88c7a6ac77d3
Manuel <manuel.gon.pos@gmail.com>
2025-06-23 18:35:28 +0200

    Formatting

--- src/test/java/com/microservices/microservices/TestBookLocator.java
+++ src/test/java/com/microservices/microservices/TestBookLocator.java
@@ -34,6 +34,8 @@ class TestBookLocator {
 		String isbnLocator = bl.generateLocator(isbn);
 		assertEquals("15443J", isbnLocator);
 
+		
+		
 		isbn = "2344512279";
 
 		concretBookSearcher = new BookSearcherInterface() {
