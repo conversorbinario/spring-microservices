581095647a8cc540458367092a30466b5cb2bc40
Manuel <manuel.gon.pos@gmail.com>
2025-07-07 17:55:46 +0200

    Simple refactor to create stub using mock.

--- src/test/java/com/microservices/microservices/TestBookLocator.java
+++ src/test/java/com/microservices/microservices/TestBookLocator.java
@@ -23,15 +23,12 @@ class TestBookLocator {
 
 	@Test
 	void isValidCode() {
+		
 		String isbn = "1544512279";
 
-		BookSearcherInterface concretBookSearcher = new BookSearcherInterface() {
-
-			@Override
-			public Book getBook(String isbn) {
-				return new Book("Julio Llamazares", "La lluvia amarilla", isbn);
-			}
-		};
+		BookSearcherInterface concretBookSearcher = mock(BookSearcherInterface.class);
+		when(concretBookSearcher.getBook(isbn)).thenReturn(new Book("Julio Llamazares", "La lluvia amarilla", isbn));
+		
 
 		BookLocatorGenerator bl = new BookLocatorGenerator();
 		bl.setBookSearcher(concretBookSearcher);
@@ -42,14 +39,10 @@ class TestBookLocator {
 		
 		
 		isbn = "2344512279";
+		concretBookSearcher = mock(BookSearcherInterface.class);
+		when(concretBookSearcher.getBook(isbn)).thenReturn(new Book("Alvaro Cunqueiro", "As crónicas do Sochantre", isbn));
 
-		concretBookSearcher = new BookSearcherInterface() {
 
-			@Override
-			public Book getBook(String isbn) {
-				return new Book("Alvaro Cunqueiro", "As crónicas do Sochantre", isbn);
-			}
-		};
 
 		bl = new BookLocatorGenerator();
 		bl.setBookSearcher(concretBookSearcher);
